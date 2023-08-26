48e00077ef7599dfd2fd064cc9550a8448046912
Karim Mohamed <komicho1996@gmail.com>
2023-08-26 13:13:12 +0300

    Feat: update navbar

--- etc/navbar.php
+++ etc/navbar.php
@@ -1,24 +1,30 @@
 <?php
 
-return [
-    [
-        'icon' => 'home-2',
-        'label' => 'Home',
-        'href' => 'obelaw.accounting.home'
-    ],
-    [
-        'icon' => 'chart-bar',
-        'label' => 'Chart Of Accounts',
-        'href' => 'obelaw.accounting.coa.index'
-    ],
-    [
-        'icon' => 'list',
-        'label' => 'Entries',
-        'href' => 'obelaw.accounting.entries.index'
-    ],
-    [
-        'icon' => 'receipt-2',
-        'label' => 'Price List',
-        'href' => 'obelaw.accounting.price_list.index'
-    ]
-];
+return new class
+{
+    public function navbar()
+    {
+        return [
+            [
+                'icon' => 'home-2',
+                'label' => 'Home',
+                'href' => 'obelaw.accounting.home'
+            ],
+            [
+                'icon' => 'chart-bar',
+                'label' => 'Chart Of Accounts',
+                'href' => 'obelaw.accounting.coa.index'
+            ],
+            [
+                'icon' => 'list',
+                'label' => 'Entries',
+                'href' => 'obelaw.accounting.entries.index'
+            ],
+            [
+                'icon' => 'receipt-2',
+                'label' => 'Price List',
+                'href' => 'obelaw.accounting.price_list.index'
+            ],
+        ];
+    }
+};
