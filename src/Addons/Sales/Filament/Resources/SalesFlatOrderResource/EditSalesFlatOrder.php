9302d74ab67df5ad8d1a290771a587dbd934fc42
Karim Mohamed <komicho1996@gmail.com>
2024-10-10 19:28:22 +0300

    Fix: update order totals after save

--- src/Addons/Sales/Filament/Resources/SalesFlatOrderResource/EditSalesFlatOrder.php
+++ src/Addons/Sales/Filament/Resources/SalesFlatOrderResource/EditSalesFlatOrder.php
@@ -4,8 +4,17 @@ namespace Obelaw\ERP\Addons\Sales\Filament\Resources\SalesFlatOrderResource;
 
 use Filament\Resources\Pages\EditRecord;
 use Obelaw\ERP\Addons\Sales\Filament\Resources\SalesFlatOrderResource;
+use Obelaw\ERP\Addons\Sales\Lib\Services\SalesOrderService;
 
 class EditSalesFlatOrder extends EditRecord
 {
     protected static string $resource = SalesFlatOrderResource::class;
+
+    public function afterSave()
+    {
+        $order = SalesOrderService::make()
+            ->order($this->getRecord());
+
+        $order->saveTotals();
+    }
 }
