041b02b18a8c4f34664e398377482fee71b58938
Karim Mohamed <komicho1996@gmail.com>
2024-12-31 23:11:57 +0200

    Feat: implement migration handling in AuditAddon and set migration path

--- src/Addons/Audit/AuditAddon.php
+++ src/Addons/Audit/AuditAddon.php
@@ -5,13 +5,14 @@ namespace Obelaw\ERP\Addons\Audit;
 use Filament\Panel;
 use Obelaw\ERP\Addons\Audit\Filament\Resources\SerialResource;
 use Obelaw\Twist\Base\BaseAddon;
+use Obelaw\Twist\Concerns\InteractsWithMigration;
+use Obelaw\Twist\Contracts\HasMigration;
 
-class AuditAddon extends BaseAddon
+class AuditAddon extends BaseAddon implements HasMigration
 {
-    public function pathMigrations()
-    {
-        return '/vendor/obelaw/erp/src/Addons/Audit/generate/migrations';
-    }
+    use InteractsWithMigration;
+
+    protected $pathMigrations = __DIR__ . '/../../../database/migrations/audit';
 
     public function register(Panel $panel): void
     {
